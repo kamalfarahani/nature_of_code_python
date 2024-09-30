397a762fe22e7c3dfacb360b44310452791607b4
Kamal <kamal.farahani90@gmail.com>
2024-10-01 00:17:26 +0330

    add random walker with tendency to move toward mouse

---
+++ ch00/exercises/3_random_walker_mouse.py
@@ -0,0 +1,69 @@
+import random
+import pygame
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Walker:
+    x: int
+    y: int
+
+    def step(
+        self,
+        mouse_x: int,
+        mouse_y: int,
+    ) -> None:
+
+        if random.random() < 0.2:
+            dx = mouse_x - self.x
+            dy = mouse_y - self.y
+            self.x += sign(dx)
+            self.y += sign(dy)
+        else:
+            self.x += random.randint(-1, 1)
+            self.y += random.randint(-1, 1)
+
+
+def sign(x: int) -> int:
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
+
+
+def main() -> None:
+    pygame.init()
+    display_width = 1000
+    display_height = 800
+    screen = pygame.display.set_mode((display_width, display_height))
+    clock = pygame.time.Clock()
+
+    walker = Walker(
+        x=display_width // 2,
+        y=display_height // 2,
+    )
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        walker.step(mouse_x, mouse_y)
+
+        pygame.draw.circle(
+            surface=screen,
+            color=(255, 255, 255),
+            center=(walker.x, walker.y),
+            radius=2,
+        )
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
