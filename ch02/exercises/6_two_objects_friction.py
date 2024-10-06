ff8b14be9c62161c2087abd4a516e2b60b3f0f31
Kamal <kamal.farahani90@gmail.com>
2024-10-06 21:49:55 +0330

    add exercise 6

---
+++ ch02/exercises/6_two_objects_friction.py
@@ -0,0 +1,134 @@
+import pygame
+
+
+class Mover:
+    position: pygame.Vector2
+    velocity: pygame.Vector2
+    acceleration: pygame.Vector2
+    mass: float
+    radius: int
+    width: int
+    height: int
+    friction_coefficient: float
+
+    def __init__(
+        self,
+        position: pygame.Vector2,
+        mass: float,
+        radius: int,
+        width: int,
+        height: int,
+        friction_coefficient: float,
+    ) -> None:
+        self.position = position
+        self.velocity = pygame.Vector2(0, 0)
+        self.acceleration = pygame.Vector2(0, 0)
+        self.mass = mass
+        self.radius = radius
+        self.width = width
+        self.height = height
+        self.friction_coefficient = friction_coefficient
+
+    def apply_force(self, force: pygame.Vector2) -> None:
+        acceleration = force / self.mass
+        self.acceleration += acceleration
+
+    def bounce_edges(self) -> None:
+        bounce = -0.9
+        if self.position.x > self.width - self.radius:
+            self.position.x = self.width - self.radius
+            self.velocity.x *= bounce
+
+        if self.position.x < self.radius:
+            self.position.x = self.radius
+            self.velocity.x *= bounce
+
+        if self.position.y > self.height - self.radius:
+            self.position.y = self.height - self.radius
+            self.velocity.y *= bounce
+
+        if self.position.y < self.radius:
+            self.position.y = self.radius
+            self.velocity.y *= bounce
+
+    def contact_edges(self) -> bool:
+        return self.position.y > self.height - self.radius - 1
+
+    def apply_friction(self) -> None:
+        if self.contact_edges():
+            friction_dir = -self.velocity.normalize()
+            friction = self.mass * self.friction_coefficient * friction_dir
+            self.apply_force(friction)
+
+    def update(self) -> None:
+        self.velocity += self.acceleration
+        self.position += self.velocity
+        self.acceleration *= 0
+
+    def show(self, surface: pygame.Surface) -> None:
+        pygame.draw.circle(
+            surface=surface,
+            color=(0, 0, 0),
+            center=(self.position.x, self.position.y),
+            radius=self.radius,
+            width=2,
+        )
+
+
+def main() -> None:
+    pygame.init()
+    display_width = 1000
+    display_height = 800
+    screen = pygame.display.set_mode((display_width, display_height))
+    clock = pygame.time.Clock()
+
+    movers = [
+        Mover(
+            position=pygame.Vector2(100, 100),
+            mass=1,
+            radius=20,
+            width=display_width,
+            height=display_height,
+            friction_coefficient=0.1,
+        ),
+        Mover(
+            position=pygame.Vector2(500, 100),
+            mass=5,
+            radius=50,
+            width=display_width,
+            height=display_height,
+            friction_coefficient=0.2,
+        ),
+    ]
+
+    gravity_force = pygame.Vector2(0, 0.5)
+
+    t = 0.0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((255, 255, 255))
+
+        if pygame.mouse.get_pressed()[0]:
+            wind_force = pygame.Vector2(0.1, 0)
+        else:
+            wind_force = pygame.Vector2(0, 0)
+
+        for mover in movers:
+            mover.apply_friction()
+            mover.apply_force(gravity_force * mover.mass)
+            mover.apply_force(wind_force)
+
+            mover.update()
+            mover.bounce_edges()
+            mover.show(screen)
+
+        pygame.display.update()
+        clock.tick(60)
+        t += 0.01
+
+
+if __name__ == "__main__":
+    main()
